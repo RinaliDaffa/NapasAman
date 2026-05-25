f3e385736953c0f853819070b072422203ca0036
Daffa Rinali <146355468+RinaliDaffa@users.noreply.github.com>
2026-05-25 21:15:46 +0700

    Update firebase_service.dart

--- lib/core/firebase/firebase_service.dart
+++ lib/core/firebase/firebase_service.dart
@@ -4,10 +4,10 @@ class FirebaseService {
   static Future<void> initialize() async {
     await Firebase.initializeApp(
       options: const FirebaseOptions(
-        apiKey: 'AIzaSyDjP9HkeuGiVfgWCo8hkrupThZo2RFEJUc',
-        appId: '1:1042339310245:android:9a9babfd4ce1c34063fb4c',
-        messagingSenderId: '1042339310245',
-        projectId: 'napasaman-fcc7b',
+        apiKey: '',
+        appId: '',
+        messagingSenderId: '',
+        projectId: '',
       ),
     );
   }
